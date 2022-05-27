7b9d58d9c52de5cabdf26610aa0e3bf935002d78
Ivy Kabere <kabereivy@gmail.com>
2022-05-27 15:32:16 +0300

    add an images model

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gallery',
-        'USER': 'daisynacharia',
+        'USER': 'daisymacharia',
         'PASSWORD': 'kabere',
     }
 }
--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+  image_name=models.CharField(max_length=30)
+  image_description=models.TextField()
+  
+
+
