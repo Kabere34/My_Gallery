e06db0699cf5bb1489c5147113e6174e7fb4d593
Ivy Kabere <kabereivy@gmail.com>
2022-05-28 12:06:36 +0300

    create search results view function

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -8,4 +8,14 @@ def homepage(request):
   images=Images.get_all_images()
   return render(request,'gallery_app/index.html',{'images':images})
 
+def search_results(request):
+  if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Images.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request,'gallery_app/search.html',{'message':message, 'images':searched_images})
+  else:
+    message = "You haven't searched for any term"
+    return render(request,'gallery_app/search.html',{'message':message})
 
