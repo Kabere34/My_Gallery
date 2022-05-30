af31519d144c9b0403f36aabe1816c940bc069f5
Ivy Kabere <kabereivy@gmail.com>
2022-05-31 00:27:56 +0300

    make changes to test file

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -5,12 +5,24 @@ from django.db import models
 class Location(models.Model):
   name = models.CharField(max_length =30)
 
+  def save_location(self):
+    self.save()
+
+  def delete_location(self):
+    self.delete
+
   def __str__(self):
     return self.name
 
 
 class Category(models.Model):
   name = models.CharField(max_length =30)
+
+  def save_category(self):
+    self.save()
+
+  def delete_category(self):
+    self.save()
 
   def __str__(self):
     return self.name
@@ -22,11 +34,18 @@ class Images(models.Model):
   image_category = models.ForeignKey(Category,on_delete=models.CASCADE, blank=True, null=True)
   image_location = models.ForeignKey(Location,on_delete=models.CASCADE, blank=True, null=True)
 
+  def save_images(self):
+    self.save()
+
+  def delete_images(self):
+    self.delete
+
   def __str__(self):
     return self.image_name
 
-  def save_images(self):
-    self.save()
+  # def test_Instance():
+
+
 
 
   @classmethod
